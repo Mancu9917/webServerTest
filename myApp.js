bb0e39075c4ff7a55ccc4e31e6d2d8993764e3b1
Francesco Mancuso <francescomancuso@MacBook-Air.home>
2021-09-26 17:30:40 +0200

    update folder

--- myApp.js
+++ myApp.js
@@ -1,7 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Express')
-})
+console.log("Hello World");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
